121c312ba01b0fc174d1514f5c626b952e1023f0
Ishit-1290 <vinee042006@gmail.com>
2022-01-25 21:14:57 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text/plain');
+    res.sendDate('Zeet Node');
+})
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+})
